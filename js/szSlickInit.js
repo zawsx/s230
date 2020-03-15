a6ca172a569f3d11e0f180e7bf4af0a776baaf81
zawsx <certain456+zx@gmail.com>
2020-03-15 22:08:18 +0000

    sf

--- js/szSlickInit.js
+++ js/szSlickInit.js
@@ -1,3 +1,4 @@
 $(document).ready(function(){ $('.myslick').slick({ centerPadding: '50px'  });  });
 $('.autoplay').slick({ slidesToShow: 3,  slidesToScroll: 1,  autoplay: true,  autoplaySpeed: 2000,});
 $('.lazy').slick({lazyLoad: 'ondemand', slidesToShow: 3, slidesToScroll: 1});
+	
